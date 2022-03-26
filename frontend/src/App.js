cd49bf3a1d571834250247619c9ec8bcaff9e3db
BENJIGAO <bhgao20014@gmail.com>
2022-03-26 12:37:35 -0400

    add object to react webcam

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+// Import dependencies
+import React, { useRef, useState, useEffect } from "react"
+import * as tf from "@tensorflow/tfjs"
+// 1. TODO - Import required model here
+// e.g. import * as tfmodel from "@tensorflow-models/tfmodel"
+import * as cocossd from "@tensorflow-models/coco-ssd"
+import Webcam from "react-webcam"
+import "./App.css"
+// 2. TODO - Import drawing utility here
+// e.g. import { drawRect } from "./utils/drawRect.js";
+import { drawRect } from './utils/drawRect'
 
 function App() {
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
+
+  // Main function
+  const runCoco = async () => {
+    // 3. TODO - Load network 
+    // e.g. const net = await cocossd.load()
+    const net = await cocossd.load()
+    
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net)
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video
+      const videoWidth = webcamRef.current.video.videoWidth
+      const videoHeight = webcamRef.current.video.videoHeight
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth
+      webcamRef.current.video.height = videoHeight
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth
+      canvasRef.current.height = videoHeight
+
+      // 4. TODO - Make Detections
+      // e.g. const obj = await net.detect(video);
+      const obj = await net.detect(video)
+      console.log(obj)
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d")
+
+      // 5. TODO - Update drawing utility
+      // drawSomething(obj, ctx)
+      drawRect(obj, ctx)
+    }
+  };
+
+  useEffect(()=>{runCoco()},[]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          muted={true} 
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
