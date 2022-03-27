79d43a7dd4625761141e31ec84a0de698a32c96a
Alex Zhu <alex@Alexs-MacBook-Pro.local>
2022-03-27 02:59:19 -0400

    added image

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./landingPage.css";
 import { useNavigate } from "react-router-dom";
 import { Button } from "antd";
+import Pic from '../assets/fruit.png'
 
 function LandingPage() {
   const navigate = useNavigate();
@@ -20,10 +21,12 @@ function LandingPage() {
           <div>
             <text className="subtext">Scan food to get started</text>
           </div>
-
           <Button type="link" onClick={handleOnSubmit}>
             Click to get started
           </Button>
+        </div>
+        <div className='picture'>
+        <img src={Pic}/>
         </div>
       </div>
     </div>
