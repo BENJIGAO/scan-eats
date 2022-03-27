2c55534f22dabb3c956e3b4833c60bb05f204588
Nathan Lai <nlai1328@gmail.com>
2022-03-27 02:00:58 -0400

    updated ui

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,91 +1,48 @@
-import React from 'react';
-import styled from 'styled-components';
-import {Link} from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
 
-const Section = styled.section`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #131313;
-`;
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  height: 70vh;
-  padding: 3rem calc((100vw - 1300px) / 2);
-
-  @media screen and (max-width: 768px) {
-    grid-grid-template-columns: 1fr;
-  }
-`;
-
-const ColumnLeft = styled.div`
-  display: flex;
-  color: #fff;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  padding: 10rem 9rem;
-
-  h1 {
-    margin-bottom: 0.5rem;
-    font-size: 2rem;
-  }
-  p {
-    margin: 2rem 0;
-    font-size: 4rem;
-    line-height: 1.1;
-  }
-`;
+import "./landingPage.css";
+import { useNavigate } from 'react-router-dom'
 
 const Button = styled.div`
-padding: 1rem 3rem;
+  margin: 5%;
+  margin-right:60%;
+  padding: 2rem;
   font-size: 1rem;
-  border: 2px solid #fff;
-  border-radius: 4px;
+  border: 3px solid #fff;
+  border-radius: 3px;
   outline: none;
   cursor: pointer;
   background: transparent;
   color: #fff;
 `;
 
-const ColumnRight = styled.div`
-  display: flex;
-  color: #fff;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  padding: 5rem 2rem;
 
-  h1 {
-    margin-bottom: 0.5rem;
-    font-size: 2rem;
-  }
-  p {
-    margin: 2rem 0;
-    font-size: 4rem;
-    line-height: 1.1;
-  }
-`;
 function LandingPage() {
+  
+    const navigate = useNavigate();
+  
+    const handleOnSubmit = () => {
+      navigate('/scan');
+    };
+  
   return (
-    <Section>
-        <Container>
-            <ColumnLeft>
-                <p>ScanEATS</p>
-                <h1>Scan food to get started</h1>
-                <Button>
-                  <Link to='/scan'>
-                    Get Started
-                  </Link>
-                </Button>
-            </ColumnLeft>
-            <ColumnRight>
-            </ColumnRight>
-        </Container>
-    </Section>
-  )
+    <div className="container">
+      <div className="header"></div>
+
+      <div className="content">
+        <div className="box">
+          <text className="scaneats">ScanEATS</text>
+          <div>
+            <text className="subtext">Scan food to get started</text>
+          </div>
+          <Button onClick={handleOnSubmit}>
+          <text style={{marginLeft:"17%"}}>Get started</text>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
--- frontend/src/components/ScanPage.js
+++ frontend/src/components/ScanPage.js
@@ -1,89 +1,59 @@
 // Import dependencies
-import React, { useRef, useState, useEffect } from "react"
+import React, { useRef, useState, useEffect } from "react";
 // 1. TODO - Import required model here
 // e.g. import * as tfmodel from "@tensorflow-models/tfmodel"
-import * as tf from "@tensorflow/tfjs"
-import * as cocossd from "@tensorflow-models/coco-ssd"
-import Webcam from "react-webcam"
-import "../App.css"
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import Webcam from "react-webcam";
+import "../App.css";
 // 2. TODO - Import drawing utility here
 // e.g. import { drawRect } from "./utils/drawRect.js";
-import { drawRect } from '../utils/drawRect'
-import styled from 'styled-components';
-
-const Section = styled.section`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #131313;
-`;
-
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  height: 70vh;
-  padding: 3rem calc((100vw - 1300px) / 2);
-
-  @media screen and (max-width: 768px) {
-    grid-grid-template-columns: 1fr;
-  }
-`;
+import { drawRect } from "../utils/drawRect";
+import styled from "styled-components";
+import { useNavigate } from 'react-router-dom'
 
 const Button = styled.div`
-padding: 1rem 3rem;
-  font-size: 1rem;
+  margin: 1%;
+  margin-top: 10%;
+  margin-left: 10%;
+  padding-top: 10%;
+  width: 80%;
+  height: 5%;
   border: 2px solid #fff;
   border-radius: 4px;
-  outline: none;
   cursor: pointer;
   background: transparent;
+  text-align: center;
   color: #fff;
 `;
 
-const ColumnLeft = styled.div`
-  display: flex;
-  color: #fff;
-  flex-direction: column;
-  justify-content: top;
-  align-items: flex-start;
-  padding: 9rem 100rem;
-
-  h1 {
-    margin-bottom: 0.5rem;
-    font-size: 2rem;
-  }
-  p {
-    margin: 2rem 0;
-    font-size: 4rem;
-    line-height: 1.1;
-  }
-`;
 
 function ScanPage() {
-  const [intervalId, setIntervalId] = useState(0)
-  const [isWebcamOn, setIsWebcamOn] = useState(false)
-  const [isFirstTime, setIsFirstTime] = useState(true)
-  const webcamRef = useRef(null)
-  const canvasRef = useRef(null)
-  const imageRef = useRef(null)
-  const imageLinkRef = useRef(null)
+  const [intervalId, setIntervalId] = useState(0);
+  const [isWebcamOn, setIsWebcamOn] = useState(false);
+  const [isFirstTime, setIsFirstTime] = useState(true);
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const imageRef = useRef(null);
+  const imageLinkRef = useRef(null);
 
-  useEffect(()=> {
-    runCoco()
-  }, [isFirstTime])
+  useEffect(() => {
+    runCoco();
+  }, [isFirstTime]);
 
   // Main function
   const runCoco = async () => {
-    // 3. TODO - Load network 
+    // 3. TODO - Load network
     // e.g. const net = await cocossd.load()
-    const net = await cocossd.load()
-    
+    const net = await cocossd.load();
+
     //  Loop and detect hands
-    setIntervalId(setInterval(() => {
-      detect(net)
-    }, 10))
-  }
+    setIntervalId(
+      setInterval(() => {
+        detect(net);
+      }, 10)
+    );
+  };
 
   const detect = async (net) => {
     // Check data is available
@@ -94,104 +64,123 @@ function ScanPage() {
       isWebcamOn
     ) {
       // Get Video Properties
-      const video = webcamRef.current.video
-      const videoWidth = webcamRef.current.video.videoWidth
-      const videoHeight = webcamRef.current.video.videoHeight
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
 
       // Set video width
-      webcamRef.current.video.width = videoWidth
-      webcamRef.current.video.height = videoHeight
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
 
       // Set canvas height and width
-      canvasRef.current.width = videoWidth
-      canvasRef.current.height = videoHeight
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
 
       // 4. TODO - Make Detections
       // e.g. const obj = await net.detect(video);
-      const obj = await net.detect(video)
+      const obj = await net.detect(video);
 
-      obj.forEach(obj => {
-        if (obj.class === 'apple' || obj.class === 'bottle') {
+      obj.forEach((obj) => {
+        if (obj.class === "apple" || obj.class === "bottle") {
           // console.log('x: ' + obj.bbox[0].toString() + ', y: ' + obj.bbox[1].toString())
           // console.log('width: ' + obj.bbox[2].toString() + ', height: ' + obj.bbox[3].toString())
           if (!imageLinkRef.current) {
-            imageRef.current.getContext('2d').drawImage(video, obj.bbox[0], obj.bbox[1], obj.bbox[2], obj.bbox[3], 0, 0, obj.bbox[2] * 1.75, obj.bbox[3])
-            imageRef.current.toBlob(blob => {
-              imageLinkRef.current = blob
-            })
+            imageRef.current
+              .getContext("2d")
+              .drawImage(
+                video,
+                obj.bbox[0],
+                obj.bbox[1],
+                obj.bbox[2],
+                obj.bbox[3],
+                0,
+                0,
+                obj.bbox[2] * 1.75,
+                obj.bbox[3]
+              );
+            imageRef.current.toBlob((blob) => {
+              imageLinkRef.current = blob;
+            });
           } else {
-            makeAPIRequest(obj.class)
+            makeAPIRequest(obj.class);
           }
-          
         }
-      })
+      });
 
       // Draw mesh
-      const ctx = canvasRef.current.getContext("2d")
+      const ctx = canvasRef.current.getContext("2d");
 
       // 5. TODO - Update drawing utility
       // drawSomething(obj, ctx)
-      drawRect(obj, ctx)
+      drawRect(obj, ctx);
     }
-  }
+  };
 
   const makeAPIRequest = async (foodName) => {
-    const formData = new FormData()
-    formData.append('image', imageLinkRef.current)
+    const formData = new FormData();
+    formData.append("image", imageLinkRef.current);
 
-    const response = await fetch('/classify/'.concat(foodName), {
+    const response = await fetch("/classify/".concat(foodName), {
       method: "POST",
-      body: formData
-    })
+      body: formData,
+    });
 
     if (response.status === 200) {
-      const text = await response.text()
-      console.log(text)
+      const text = await response.text();
+      console.log(text);
     } else {
-      console.log('Error with POST request')
+      console.log("Error with POST request");
     }
-  }
+  };
 
   const toggleCamera = () => {
-    setIsWebcamOn(!isWebcamOn)
+    setIsWebcamOn(!isWebcamOn);
     if (isFirstTime) {
-      setIsFirstTime(false)
+      setIsFirstTime(false);
     }
-  }
+  };
+
+  const navigate = useNavigate();
+  
+  const handleOnSubmit = () => {
+    navigate('/');
+  };
 
   return (
-    <div className="App" style={{ display: 'flex', flexDirection: 'column' }}>
-      <header className="App-header">
-        {isWebcamOn &&
-          <>
-            <Webcam
-              ref={webcamRef}
-              muted={true} 
-              style={{
-                position: "absolute",
-                top: 250,
-                left: 200,
-                zindex: 9,
-                width: 640,
-                height: 480,
-              }}
-            />
-            <canvas
-              ref={canvasRef}
-              style={{
-                position: "absolute",
-                top: 250,
-                left: 200,
-                zindex: 8,
-                width: 640,
-                height: 480,
-              }}
-            />
-          </>
-        }
-        
-      </header>
-      <canvas
+    <div>
+    <div className="container">
+      <div className="header"></div>
+      <div className="content">
+        <header className="App-header">
+          {isWebcamOn && (
+            <>
+              <Webcam
+                ref={webcamRef}
+                muted={true}
+                style={{
+                  position: "absolute",
+                  top: 250,
+                  left: 200,
+                  zindex: 9,
+                  width: 640,
+                  height: 480,
+                }}
+              />
+              <canvas
+                ref={canvasRef}
+                style={{
+                  position: "absolute",
+                  top: 250,
+                  left: 200,
+                  zindex: 8,
+                  width: 640,
+                  height: 480,
+                }}
+              />
+            </>
+          )}
+        </header>
+        <canvas
           ref={imageRef}
           style={{
             position: "absolute",
@@ -201,16 +190,17 @@ function ScanPage() {
             width: 480,
             height: 480,
           }}
-      />
-      <Section>
-        <Container>
-          <ColumnLeft>
-            <Button onClick={toggleCamera}>
-              {isWebcamOn ? 'Turn Camera off' : 'Turn Camera On'}
-            </Button>
-          </ColumnLeft>
-        </Container>
-      </Section>
+        />
+        <div className= "buttons">
+        <Button onClick={handleOnSubmit}>
+          Home
+        </Button>
+        <Button onClick={toggleCamera}>
+          {isWebcamOn ? "Turn Camera Off" : "Turn Camera On"}
+        </Button>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
